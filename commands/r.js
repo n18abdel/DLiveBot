b6e7eb3490e1c99fa31836d0ec95e72020a3362e
Nassim <nassim.abdelghani@imt-atlantique.net>
2021-06-19 11:44:22 +0200

    add feature

---
+++ commands/r.js
@@ -0,0 +1,84 @@
+const https = require("https");
+const moment = require("moment-timezone");
+const { getUsername } = require("../helpers/request");
+
+const commandData = {
+  name: "r",
+  description: "r",
+  options: [
+    {
+      name: "s",
+      type: "STRING",
+      description: "s",
+      required: true,
+    },
+  ],
+  defaultPermission: false,
+};
+
+const range = (size, startAt) =>
+  [...Array(size).keys()].map((i) => i + startAt);
+
+const computeUrl = (username, timestamp, { end = true } = {}) => {
+  if (end) {
+    return `https://playback.prd.dlivecdn.com/live/${username}/${timestamp}/master/stitched-playlist.m3u8`;
+  }
+  return `https://playback.prd.dlivecdn.com/live/${username}/${timestamp}/vod.m3u8`;
+};
+
+const func = async ({ interaction, guildId, args, botState }) => {
+  const { lastStreams } = botState;
+
+  const { s } = args;
+
+  getUsername(s).then((username) => {
+    const beginningTimestamp = moment(
+      Number(lastStreams[username][guildId].createdAt)
+    ).unix();
+    const endingTimestamp = moment(
+      Number(lastStreams[username][guildId].finishedAt)
+    ).unix();
+
+    Promise.any(
+      range(60, -15).forEach(
+        (i) =>
+          new Promise((resolve, reject) => {
+            const url = computeUrl(username, endingTimestamp + i);
+            https.get(url, (res) => {
+              if (res.statusCode === 200) {
+                resolve(url);
+              }
+              reject();
+            });
+          })
+      )
+    )
+      .catch(() =>
+        Promise.any(
+          range(60, -15).forEach(
+            (i) =>
+              new Promise((resolve, reject) => {
+                const url = computeUrl(username, beginningTimestamp + i, {
+                  end: false,
+                });
+                https.get(url, (res) => {
+                  if (res.statusCode === 200) {
+                    resolve(url);
+                  }
+                  reject();
+                });
+              })
+          )
+        )
+      )
+      .then((url) => {
+        interaction.reply(url);
+      })
+      .catch(() => interaction.reply("nothing there"));
+  });
+};
+
+module.exports = {
+  commandData,
+  func,
+};
